5d254b5f839380b7d89b26e4640252a0a31683e7
Dinis <dinisvferreira@gmail.com>
2024-02-23 10:30:51 +0000

    add variáveis inteiras

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          sbyte sb = -70;
+          short sh = -15634;
+          int inter = -2147483647;
+          long lo = -9223372036854775807;
+          byte by = 125;
+          ushort us = 12555;
+          uint ui = 14785;
+          ulong lon = 180000000;
+          char cha =  'N';
+
+            
+            
+            
+            
+            
+            Console.WriteLine(sb);
+            Console.WriteLine(sh);
+            Console.WriteLine(inter);
+            Console.WriteLine(lo);
+            Console.WriteLine(by);
+            Console.WriteLine(us);
+            Console.WriteLine(ui);
+            Console.WriteLine(lon);
+            Console.WriteLine(cha);
+        }
+    }
+}
