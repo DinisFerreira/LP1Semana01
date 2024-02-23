8eb2071d84505f48da96d4fba9b8f63c7a127e06
Dinis <dinisvferreira@gmail.com>
2024-02-23 11:22:14 +0000

    add Strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Um Null\0 e uma copyrightSymbol\u00A9";
+            string mark ="é um Carriage return newLine \u000A";
+
+            Console.WriteLine(message);
+            Console.WriteLine(mark);
+        }
+    }
+}
