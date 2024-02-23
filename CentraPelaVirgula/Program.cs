720150c80150b19d4e75781177af97b00b322f43
Dinis <dinisvferreira@gmail.com>
2024-02-23 14:37:02 +0000

    add 4 numeros decimais alinhados a direita com 2 casas decimais

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escreva 4 numeros reais:");
+            string azul = Console.ReadLine();
+            string verde = Console.ReadLine();
+            string rosa = Console.ReadLine();
+            string preto = Console.ReadLine();
+
+        
+            float cima = float.Parse(azul);
+            float baixo = float.Parse(verde);
+            float esquerda = float.Parse(rosa);
+            float direita = float.Parse(preto);
+
+           
+            string norte = $"{cima,6:f2}";
+            string sul = $"{baixo,6:f2}";
+            string este = $"{esquerda,6:f2}";
+            string oeste = $"{direita,6:f2}";
+
+            
+            Console.WriteLine(norte);
+            Console.WriteLine(sul);
+            Console.WriteLine(este);
+            Console.WriteLine(oeste);
+        }
+    }
+}
