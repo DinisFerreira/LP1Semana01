f18672e072ca14c83e00f438935a976147f8ea27
Dinis <dinisvferreira@gmail.com>
2024-02-23 11:59:38 +0000

    add interpolação e concatenação

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -8,9 +8,16 @@ namespace ManyStrings
         {
             string message = "Um Null\0 e uma copyrightSymbol\u00A9";
             string mark ="é um Carriage return newLine \u000A";
-
+            string tour = "a" + 2;
+            string lol = "abc" + 'x';
+            int x = 5;
+            int y = 7;
+            
             Console.WriteLine(message);
             Console.WriteLine(mark);
+            Console.WriteLine(tour);
+            Console.WriteLine(lol);
+            Console.WriteLine($"{x} mais {y} e igual a {x + y}");
         }
     }
 }
