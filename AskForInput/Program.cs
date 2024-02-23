c758ef22252c33ebf0fadf85a579003f6abb1488
Dinis <dinisvferreira@gmail.com>
2024-02-23 14:15:50 +0000

    add soma dos 2 numeros

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -9,7 +9,12 @@ namespace AskForInput
             Console.WriteLine("insira um numero inteiro");
             string malta = Console.ReadLine();
             int boas = int.Parse(malta);
-            
+            Console.WriteLine("insira um numero Real");
+            string Ler = Console.ReadLine();
+            float fado = float.Parse(Ler);
+            Console.WriteLine(boas+fado);
+
+
         }
     }
 }
