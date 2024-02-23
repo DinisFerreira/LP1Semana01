20a07c7fc685d58001bf672d9ca359c2c0a8063b
Dinis <dinisvferreira@gmail.com>
2024-02-23 09:39:25 +0000

    add summary

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 
 namespace MeuPrograma
-{/// Escrever uma frase de boas vindas>
+{
+
+
+/// Escrever uma frase de boas vindas>
 /// usamos o comando  Console.WriteLine para escrever essas frases
 /// </Escrever uma frase de despedida>
-
     class Program
     {
         static void Main(string[] args)
