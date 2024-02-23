d87de4d2d6001b3e0a0cd74ad74f773ce9e2bb13
Dinis <dinisvferreira@gmail.com>
2024-02-23 09:04:29 +0000

    Escrevi uma frase de boas vindas

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 
 namespace MeuPrograma
-{
+{/// Escrever uma frase de boas vindas>
+/// usamos o comando  Console.WriteLine para escrever essas frases
+/// </Escrever uma frase de despedida>
+
     class Program
     {
         static void Main(string[] args)
